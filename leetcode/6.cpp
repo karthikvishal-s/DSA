3ca1456ef5a259e69c1a9a76aec0079d5e52e5c5
Karthik Vishal <karthikvishal1506@gmail.com>
2025-07-23 21:35:26 +0530

    zigzag sum revisited and edited optimally

--- leetcode/6.cpp
+++ leetcode/6.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-class Solution {
-public:
-    string convert(string s, int numRows) {
-        if (numRows == 1 || numRows >= s.length())
-            return s;
+int main(void) {
+    string s = "PAYPALISHIRING";
+    int numRows = 3;
 
-        string res = "";
-        int increment = 2 * (numRows - 1);
+    if (numRows == 1 || numRows >= s.length()) {
+        cout << s;
+        return 0;
+    }
 
-        for (int r = 0; r < numRows; r++) {
-            for (int i = r; i < s.length(); i += increment) {
-                res += s[i];
+    string res = "";
+    int increment = 2 * (numRows - 1);
 
-                // For middle rows, add the diagonal character
-                int diagIndex = i + increment - 2 * r;
-                if (r > 0 && r < numRows - 1 && diagIndex < s.length()) {
-                    res += s[diagIndex];
-                }
+    for (int r = 0; r < numRows; r++) {
+        for (int i = r; i < s.length(); i += increment) {
+            res += s[i];
+
+            // For middle rows, add the diagonal character
+            int diagIndex = i + increment - 2 * r;
+            if (r > 0 && r < numRows - 1 && diagIndex < s.length()) {
+                res += s[diagIndex];
             }
         }
-
-        return res;
     }
-};
+
+    cout << res;
+    return 0;
+}
