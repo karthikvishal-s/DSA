b76b1ae5a6209d158384a1347b8846e47fff5e15
Karthik Vishal <karthikvishal1506@gmail.com>
2025-09-06 20:15:40 +0530

    Completed Stick with O(n)

---
+++ py_heaps/connectSticks.py
@@ -0,0 +1,20 @@
+import heapq
+
+sticks = [2,4,3]
+
+
+minheap=sticks
+heapq.heapify(minheap)
+cost = 0
+
+while len(minheap)>1:
+    first = heapq.heappop(minheap)
+
+    second = heapq.heappop(minheap)
+
+    cost = first+second
+ 
+    heapq.heappush(minheap,first+second)
+
+
+print(cost)
