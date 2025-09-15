cbca88e3991c176f248424be797f660f8ce74b49
Karthik Vishal <karthikvishal1506@gmail.com>
2025-09-15 10:34:28 +0530

    Added las stone weight

---
+++ py_heaps/lastStoneweight.py
@@ -0,0 +1,22 @@
+import heapq
+
+stones=[2,7,4,1,8,1]
+
+
+max_heap=[-n for n in stones]
+heapq.heapify(max_heap)
+
+while len(max_heap)>1:
+    s1=-heapq.heappop(max_heap)
+    s2=-heapq.heappop(max_heap)
+    if s1>s2:
+        heapq.heappush(max_heap,-(s1-s2))
+    
+
+    print(max_heap)
+
+if max_heap:
+    print( -heapq.heappop(max_heap))
+
+else:
+    print(0)
