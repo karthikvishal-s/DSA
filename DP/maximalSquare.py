ac4fc668278f02275e6d60e1bfea2f2a0ccf01a9
Karthik Vishal <karthikvishal1506@gmail.com>
2025-09-19 00:00:24 +0530

    done maximal square

--- DP/maximalSquare.py
+++ DP/maximalSquare.py
@@ -14,17 +14,14 @@ matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1"
 m=len(matrix)
 n=len(matrix[0])
 dp=[[0]*(n+1) for _ in range(m+1)]
+maxn=1
 
 
 for i in range(m-1,-1,-1):
     for j in range(n-1,-1,-1):
-        if matrix[i][j]==0:
-            dp[i][j]=0
-            continue
-
-        if dp[i][j+1]==1 & dp[i+1][j+1]==1 & dp[i+1][j]==1:
-            dp[i][j] = dp[i][j]+1
-
-
+        if matrix[i][j] == "1":
+                dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+                maxn = max(maxn, dp[i][j])
 
 print(dp)
+print(maxn*maxn)
